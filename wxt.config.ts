3b0d409444c10dcb513e7cb5df1887e2bc3bd8da
DraconDev <dracsharp@gmail.com>
2025-03-14 20:26:20 +0000

    chore: update bun.lockb

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.26",
+    version: "0.0.27",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
