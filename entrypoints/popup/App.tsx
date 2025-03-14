7ef1b2c58c67fa546234eec4e2eb3cc2826a58cb
DraconDev <dracsharp@gmail.com>
2025-03-14 20:32:40 +0000

    fix(popup): remove unused color class Removes the `color-red-900` class as it s redundant with `red-900` in the popup component.

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen p-4 bg-gray-100 color-red-900">hello</div>
+      <div className="min-h-screen p-4 bg-gray-100 red-900">hello</div>
     </>
   );
 }
