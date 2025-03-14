6a1e93ba2994ef187a179d808aa52b5afdcf436a
DraconDev <dracsharp@gmail.com>
2025-03-14 20:19:29 +0000

    fix(popup): correct css import path

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.18",
+    version: "0.0.19",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
