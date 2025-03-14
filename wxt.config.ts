8d1b986c30f9f3591ac831e01c2de1e3a9613511
DraconDev <dracsharp@gmail.com>
2025-03-14 20:49:45 +0000

    feat(deps): upgrade @types/chrome to 0.0.309

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.41",
+    version: "0.0.42",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
