99f8a59af60145f390d3bf0038afb0837adc10fe
DraconDev <dracsharp@gmail.com>
2025-03-14 11:50:50 +0000

    feat(config): add Firefox compatibility This commit updates the wxt config to target Firefox by: - Setting `extensionApi` to  webextension . - Adding  firefox  to the `browser` array.

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,30 +1,30 @@
 import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    extensionApi: "webextension", // Changed from "chrome" to "webextension" for better Firefox compatibility
-    browser: ["chrome", "firefox"], // Add Firefox as a target browser
-    manifest: {
-        name: "Example Extension",
-        description: "Example Extension",
-        version: "0.0.3",
-        permissions: ["storage"],
-        icons: {
-            "16": "icon/16.png",
-            "32": "icon/32.png",
-            "48": "icon/48.png",
-            "96": "icon/96.png",
-            "128": "icon/128.png",
-        },
-        action: {
-            default_popup: "popup.html",
-            default_icon: {
-                "16": "icon/16.png",
-                "32": "icon/32.png",
-                "48": "icon/48.png",
-                "128": "icon/128.png",
-            },
-            default_title: "",
-        },
+  extensionApi: "webextension", // Changed from "chrome" to "webextension" for better Firefox compatibility
+  browser: ["chrome", "firefox"], // Add Firefox as a target browser
+  manifest: {
+    name: "Example Extension",
+    description: "Example Extension",
+    version: "0.0.4",
+    permissions: ["storage"],
+    icons: {
+      "16": "icon/16.png",
+      "32": "icon/32.png",
+      "48": "icon/48.png",
+      "96": "icon/96.png",
+      "128": "icon/128.png",
     },
-    modules: ["@wxt-dev/module-react"],
+    action: {
+      default_popup: "popup.html",
+      default_icon: {
+        "16": "icon/16.png",
+        "32": "icon/32.png",
+        "48": "icon/48.png",
+        "128": "icon/128.png",
+      },
+      default_title: "",
+    },
+  },
+  modules: ["@wxt-dev/module-react"],
 });
