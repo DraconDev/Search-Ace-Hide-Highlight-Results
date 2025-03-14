e5a52f95e1ae1c87b19c9e013f3cf9bd31f1e5fb
DraconDev <dracsharp@gmail.com>
2025-03-14 20:28:16 +0000

    fix(dev): specify chrome path for development

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.29",
+    version: "0.0.30",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
