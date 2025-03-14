b22633de38cdafe852d50d962ad03174c8ee5f2b
DraconDev <dracsharp@gmail.com>
2025-03-14 20:20:43 +0000

    fix(deps): update @types/chrome to 0.0.309

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.23",
+    version: "0.0.24",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
