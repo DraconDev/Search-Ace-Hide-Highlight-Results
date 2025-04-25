5bc27cb485f536834e654216bd451d55ead018b9
DraconDev <dracsharp@gmail.com>
2025-04-25 06:28:22 +0100

    feat(store): add hidden and highlighted results to store

--- utils/store.ts
+++ utils/store.ts
@@ -2,6 +2,8 @@ import { Store } from "@/types/types";
 
 export const defaultStore: Store = {
     env: "",
+    hiddenResults: [],
+    highlightedResults: [],
 };
 
 export const store = storage.defineItem<Store>("sync:store", {
