50771995957ea4c670242ac9491cdf07905edcbf
DraconDev <dracsharp@gmail.com>
2025-04-25 07:00:19 +0100

    feat(popup): Update popup title to  Custom Search Results

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.76",
+    version: "0.0.77",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
