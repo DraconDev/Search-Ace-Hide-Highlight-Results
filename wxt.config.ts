b397da06f4222a9a53fb17bacaff20c09a960ecd
DraconDev <dracsharp@gmail.com>
2025-04-25 08:27:39 +0100

    feat(content): Add highlight and hide buttons to search results

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.172",
+    version: "0.0.173",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
