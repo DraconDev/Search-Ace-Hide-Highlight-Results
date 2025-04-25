ae74a4204899d2fe53a2775af34783c0101c1c89
DraconDev <dracsharp@gmail.com>
2025-04-25 07:52:45 +0100

    feat(store): add defaultHighlightColor to store type

--- types/types.ts
+++ types/types.ts
@@ -2,5 +2,6 @@ export type Store = {
     env: string;
     hiddenResults: { [pattern: string]: boolean };
     highlightedResults: { [pattern: string]: string };
+    defaultHighlightColor: string;
     suspended: boolean;
 };
--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.142",
+    version: "0.0.143",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
