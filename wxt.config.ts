f901fd4b26e9aa31b27d759b16c96acd257dcc66
DraconDev <dracsharp@gmail.com>
2025-04-25 08:37:38 +0100

    feat(content): Apply immediate highlight style to search results

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Example Extension",
     description: "Example Extension",
-    version: "0.0.184",
+    version: "0.0.185",
     permissions: ["storage"],
     icons: {
       "16": "icon/16.png",
